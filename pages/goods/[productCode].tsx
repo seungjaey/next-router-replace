7d40ef2e9c0b3bfeb2493b48a61693baed96d288
seungjaey <seungjaey@gmail.com>
2022-09-04 17:43:46 +0900

    상품상세 replace

---
+++ pages/goods/[productCode].tsx
@@ -0,0 +1,64 @@
+import type { NextPage, GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const TEST_LINKS = [
+  {
+    code: 1,
+    label: "테스트"
+  },
+  {
+    code: 2,
+    label: "테스트"
+  },
+  {
+    code: 3,
+    label: "테스트"
+  },
+]
+
+const GoodsDetailPage: NextPage = ({ productCode }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const handleClickLink = (code: number) => () => {
+    if (productCode === code) {
+      return;
+    }
+    (async () => {
+      await router.replace(
+        "/goods/[productCode]",
+        `/goods/${code}`
+      )
+    })()
+  }
+  return (
+    <div>
+      <h1>{`${productCode} Page`}</h1>
+      <ul>
+        {
+          TEST_LINKS.map((item) => {
+            const {code, label} = item
+            return (
+              <li key={`${code}-item`}>
+                <button onClick={handleClickLink(code)}>
+                  {`${code} - ${label}`}
+                </button>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default GoodsDetailPage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { query } = context
+  const { productCode } = query
+  return {
+    props: {
+      productCode,
+    }
+  }
+}
